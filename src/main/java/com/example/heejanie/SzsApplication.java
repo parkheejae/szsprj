7de972fd3acfccfacbcb62ac95442422d02ea4f6
daecadid <daecadid@gmail.com>
2021-12-24 22:02:14 +0900

    [과제 프로젝트] 기본 설정 spring boot/h2/jpa

---
+++ src/main/java/com/example/heejanie/SzsApplication.java
@@ -0,0 +1,13 @@
+package com.example.heejanie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heejanie/SzsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heejanie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
